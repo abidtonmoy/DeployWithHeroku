a6defa628871389a6b97cb96d63e317e0514edcc
Abidtonmoy007 <abidtonmoy45@gmail.com>
2024-01-27 14:21:20 +0600

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header>
       <h1>Abid Hasan</h1>
-      <p>Deploying test using heroku</p>
+      <p>Deploying test using netlify</p>
     </header>
   );
 };
